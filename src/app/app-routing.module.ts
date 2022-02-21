849366cb50e7958c613446b266e6d8fa08df6dfd
Nero1900 <nerobike1990@hotmail.com>
2022-02-21 21:55:08 +0100

    Adding RoutingLinks for Main-Interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ThreadComponent } from './thread/thread.component';
+import { MainInterfaceComponent } from './main-interface/main-interface.component';
 
 const routes: Routes = [
 {path:'', component:LoginComponent},
 {path:'login', component:LoginComponent},
-{path:'register', component:RegisterComponent}
+{path:'register', component:RegisterComponent},
+{path:'login/main-interface', component:MainInterfaceComponent}
 ];
 
 @NgModule({
