12a6befb3ac1a22eb0ea34ca90a50ead6380ee70
Daniel Helfenstein <d.helfenstein@mail.de>
2022-02-27 16:42:26 +0100

    implementing the login and registration part

--- src/app/backend-service.service.ts
+++ src/app/backend-service.service.ts
@@ -1,55 +1,38 @@
 import { Injectable } from '@angular/core';
+import { async } from '@angular/core/testing';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { timeout } from 'rxjs';
 import { User } from 'src/models/user.class';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendServiceService {
-  user!: User;
-  tempDoc2: string[] = [];
+  
+  elementArray:string[] = [];
+ 
 
   constructor(public database:AngularFirestore) { }
-
-
-
 
     public saveToDatabase(category:string, objectToSave:any){   
       this.database
       .collection(category)
       .add(objectToSave)
       .then((result: any) => {
-     
         console.log('Adding finished', result);
-    
       });
     }
 
 
     public getDataFormDatabase(category:string){
         let tempDoc: string[] = [];
-        this.database.collection(category).valueChanges().subscribe((users: any)=>{
-          users.forEach(async (doc:string)=>{
-            let users: string = doc;
-            this.tempDoc2.push(users);
-            tempDoc.push(users);
-            //console.log(doc);
-            
-            
-
-            
-           
-           
-          });
-          //console.log("tempDocInnenInnen: ",tempDoc[0]);
+        this.database.collection(category).valueChanges().subscribe((collection: any)=>{
+            collection.forEach((element:string)=>{
+           tempDoc.push(element);
+            });
+              this.elementArray = tempDoc;
         });
-        
-        //console.log("tempDocInnenAussen: ",tempDoc[0]);
-
-        //console.log("tempdocAussen: ",this.tempDoc2[0]);
-        return this.tempDoc2;
-        
-        
+       
     }
 
         
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BackendServiceService } from '../backend-service.service';
 
 @Component({
@@ -7,37 +8,61 @@ import { BackendServiceService } from '../backend-service.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  inputPassword!:string;
+  inputUsername!:string;
+ 
 
-  constructor(public backend:BackendServiceService) { }
+
+  constructor(public backend:BackendServiceService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * this method manages the login: get the data form the backend and let the user go the main-interface
+   */
   logIn() {
-    /*setTimeout(async ()=>{
+  if(this.inputPassword !=='' || this.inputPassword== undefined && this.inputUsername== undefined || this.inputUsername !==''){
+    let testArray:any [] = [];
+    this.backend.getDataFormDatabase('users');
+    setTimeout(()=>{
+      if(this.checkCredentials(this.backend.elementArray)){
+        this.router.navigate(['login/main-interface']);
+      }else{
+        alert('Password or Username are incorrect try again');
+      };
+      },500);
      
-     
-
-    },5000);
-*/
-  let testArray:string [] = this.backend.getDataFormDatabase('users');
-  console.log("hier spricht das testarray", testArray);
-    
-  } 
-    
-
-    
-
-    
-
-
-
-
-
+    }
+  }
   
 
 
-
+  /**
+   * this method checks if one user has the same credentials which were typed in
+   * @param testArray 
+   * @returns 
+   */
+  checkCredentials(testArray:any[]){
+    for (let i = 0; i < testArray.length; i++) {
+      console.log(testArray[i]);
+      
+      if(testArray[i].password === this.inputPassword &&
+        testArray[i].userName === this.inputUsername){
+          return true;
+      }else{
+        return false;
+        
+      }
+    }
+    return false;
+  }
+    
   
+
 
 }
+
+
+
+
